a40143acda8d898f60d074c9fb5797f60d6c10cf
Gavin-John Noonan <gavinjohn.noonan@gmail.com>
2015-08-27 21:33:10 +0100

    Create some initial app state
    As well as start fleshing out the data we want. This may change, but it
    is enough to get us started.

--- src/cljs/duke/app.cljs
+++ src/cljs/duke/app.cljs
@@ -2,13 +2,20 @@
   (:require [om.core :as om :include-macros true]
             [sablono.core :as html :refer-macros [html]]))
 
+(defonce initial-state
+  (atom {:playlist [{:id 1 :track "Wait and Bleed" :artist "Slipknot" :album "Slipknot"}
+                    {:id 2 :track "Zzyzx Rd." :artist "Stone Sour" :album "Come What(ever) May"}
+                    {:id 3 :track "Learn To Fly" :artist "Foo Fighters" :album "There Is Nothing Left To Lose"}
+                    {:id 4 :track "Viking Death March" :artist "Billy Talent" :album "Dead Silence"}]
+         :currently-playing 2}))
+
 (defn widget [data owner]
   (reify
     om/IRender
     (render [this]
       (html
-       [:h2 (:text data)]))))
+       [:h2 (:currently-playing data)]))))
 
 (defn init []
-  (om/root widget {:text "This is duke, prepare to be amazed"}
+  (om/root widget initial-state
            {:target (. js/document (getElementById "container"))}))
